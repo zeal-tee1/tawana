00f73e99d4fbaa234ccff266dfe6b5c3786f5c72
zeal-tee1 <tawandabundukutu@gmail.com>
2020-08-30 18:25:51 +0100

    zeal

---
+++ src/main/java/com/tess/demo/TestestApplication.java
@@ -0,0 +1,13 @@
+package com.tess.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tess/demo/TestestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tess.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
